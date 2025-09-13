7934d97a190e265682995c059c975114e6833a1d
ABHISHEK KUMAR RAJ <24mcmc24@uohyd.ac.in>
2025-09-13 16:29:58 +0000

    Understanding our data by for loop

--- PythonFile/MainModel.py
+++ PythonFile/MainModel.py
@@ -11,3 +11,19 @@ from sklearn.preprocessing import MinMaxScaler
 
 # pip install xgboost
 # pip install graphviz
+
+raw_data = pd.read_csv("Churn_Modelling.csv",encoding="latin1")
+
+# Displaying the 1st 5 row
+print(raw_data.head())
+
+# Understanding Our data
+# Investigate all the elements within each feature
+
+for column in raw_data:
+    unique_vals = np.unique(raw_data[column].fillna('0'))
+    nr_values = len(unique_vals)
+    if nr_values <= 12:
+        print('The number of values for feature {} : {} -- {}'.format(column,nr_values,unique_vals))
+    else:
+        print('The number of values for feature {} : {}'.format(column, nr_values))
